21a6c6231935dda7e6f4f2a83e17d339b2d70a77
James Clancey <james.clancey@gmail.com>
2019-06-24 12:15:22 -0800

    Fixed sample

--- src/HotUI/Controls/ListView.cs
+++ src/HotUI/Controls/ListView.cs
@@ -1,157 +1,197 @@
-﻿//using System;
-//using System.Collections;
-//using FControlType = Xamarin.Forms.ListView;
-//using Template = Xamarin.Forms.DataTemplateSelector;
-//using FView = Xamarin.Forms.View;
-//using FCell = Xamarin.Forms.Cell;
-//using VCell = Xamarin.Forms.ViewCell;
-//using Xamarin.Forms;
-//using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
-//namespace HotUI {
+namespace HotUI {
 
-//	public class ListView : ListView<object>, IEnumerable {
-//		public IEnumerator GetEnumerator () => FormsControl.ItemsSource.GetEnumerator ();
+	public class ListView<T> : View, IEnumerable, IEnumerable<Func<T, View>> {
+		public ListView (IList<T> list)
+		{
+			List = list;
+		}
 
-//		List<FCell> views = new List<FCell> ();
+		public IList<T> List { get; }
+		public IEnumerator GetEnumerator () => List.GetEnumerator ();
+		Func<object, View> cellCreator;
+		public void Add (Func<object, View> viewCreator)
+		{
+			if (cellCreator != null) {
+				throw new Exception ("You can only have one View Creator");
+			}
+			cellCreator = viewCreator;
+		}
 
-//		public void Add (FView view)
-//		{
-//			if (view == null)
-//				return;
-//			views.Add (new VCell {
-//				View = view,
-//			});
-//		}
+		IEnumerator<Func<T, View>> IEnumerable<Func<T, View>>.GetEnumerator ()
+		{
+			throw new NotImplementedException ();
+		}
+	}
 
-//		//public void Add (FCell view)
-//		//{
-//		//	if (view == null)
-//		//		return;
+	public class ListView : View, IEnumerable, IEnumerable<Func<object,View>> {
+		public ListView(IList list)
+		{
+			List = list;
+		}
 
-//		//	views.Add (view);
-//		//}
+		public IList List { get; }
+		public IEnumerator GetEnumerator () => List.GetEnumerator ();
+		Func<object, View> cellCreator;
+		public void Add(Func<object,View> viewCreator)
+		{
+			if(cellCreator != null) {
+				throw new NotImplementedException ("You can only have one View Creator");
+			}
+			cellCreator = viewCreator;
+		}
 
-//		void CheckCanAddView ()
-//		{
-//			if (ViewFor != null && views.Count > 0)
-//				throw new Exception ("You cannot add Views and a ViewFor");
-//			if (views.Count > 0 && ItemsSource != null)
-//				throw new Exception ("You cannot add Views and use an ItemSource");
+		IEnumerator<Func<object, View>> IEnumerable<Func<object, View>>.GetEnumerator ()
+		{
+			throw new NotImplementedException ();
+		}
+	}
 
-//		}
+	//public class ListView : ListView<object>, IEnumerable {
+	//	public IEnumerator GetEnumerator () => FormsControl.ItemsSource.GetEnumerator ();
 
-//		public void Add (View view)
-//		{
-//			if (view == null)
-//				return;
-//			views.Add (new VCell { View = view });
-//		}
+	//	List<FCell> views = new List<FCell> ();
+
+	//	public void Add (FView view)
+	//	{
+	//		if (view == null)
+	//			return;
+	//		views.Add (new VCell {
+	//			View = view,
+	//		});
+	//	}
+
+	//	//public void Add (FCell view)
+	//	//{
+	//	//	if (view == null)
+	//	//		return;
+
+	//	//	views.Add (view);
+	//	//}
+
+	//	void CheckCanAddView ()
+	//	{
+	//		if (ViewFor != null && views.Count > 0)
+	//			throw new Exception ("You cannot add Views and a ViewFor");
+	//		if (views.Count > 0 && ItemsSource != null)
+	//			throw new Exception ("You cannot add Views and use an ItemSource");
+
+	//	}
+
+	//	public void Add (View view)
+	//	{
+	//		if (view == null)
+	//			return;
+	//		views.Add (new VCell { View = view });
+	//	}
 
 
-//		protected override object CreateFormsView ()
-//		{
-//			CheckCanAddView ();
+	//	protected override object CreateFormsView ()
+	//	{
+	//		CheckCanAddView ();
 
-//			var list = (FControlType)base.CreateFormsView ();
-//			if(views.Count > 0) {
-//				list.ItemTemplate = new CellTemplate ();
-//			}
-//			return list;
-//		}
+	//		var list = (FControlType)base.CreateFormsView ();
+	//		if (views.Count > 0) {
+	//			list.ItemTemplate = new CellTemplate ();
+	//		}
+	//		return list;
+	//	}
 
-//		class CellTemplate : Template {
-//			protected override DataTemplate OnSelectTemplate (object item, BindableObject container) =>
-//				new DataTemplate (() => (Cell)item);
-//		}
-//	}
+	//	class CellTemplate : Template {
+	//		protected override DataTemplate OnSelectTemplate (object item, BindableObject container) =>
+	//			new DataTemplate (() => (Cell)item);
+	//	}
+	//}
 
 
-//	public class ListView<T> : View<FControlType>  {
+	//public class ListView<T> : View<FControlType> {
 
-//		public IEnumerable ItemsSource { get; set; }
-//		public Func<T,FView> ViewFor { get; set; }
-//		public Action<T> ItemSelected { get; set; }
+	//	public IEnumerable ItemsSource { get; set; }
+	//	public Func<T, FView> ViewFor { get; set; }
+	//	public Action<T> ItemSelected { get; set; }
 
-//		protected override object CreateFormsView ()
-//		{
-//			var control = (FControlType)base.CreateFormsView ();
-//			control.HasUnevenRows = true;
-//			if(ItemsSource != null) {
-//				control.ItemsSource = ItemsSource;
-//			}
-//			if (ViewFor != null)
-//				control.ItemTemplate = new ListViewCellTemplate<T> { CellFor = ViewFor };
-//			if(ItemSelected != null)
-//				control.ItemSelected += Control_ItemSelected;
-//			return control;
-//		}
+	//	protected override object CreateFormsView ()
+	//	{
+	//		var control = (FControlType)base.CreateFormsView ();
+	//		control.HasUnevenRows = true;
+	//		if (ItemsSource != null) {
+	//			control.ItemsSource = ItemsSource;
+	//		}
+	//		if (ViewFor != null)
+	//			control.ItemTemplate = new ListViewCellTemplate<T> { CellFor = ViewFor };
+	//		if (ItemSelected != null)
+	//			control.ItemSelected += Control_ItemSelected;
+	//		return control;
+	//	}
 
-//		protected override void UnbindFormsView (object formsView)
-//		{
+	//	protected override void UnbindFormsView (object formsView)
+	//	{
 
-//		}
+	//	}
 
-//		protected override void UpdateFormsView (object formsView)
-//		{
+	//	protected override void UpdateFormsView (object formsView)
+	//	{
 
-//		}
+	//	}
 
-//		private void Control_ItemSelected (object sender, SelectedItemChangedEventArgs e)
-//		{
-//			ItemSelected?.Invoke ((T)e.SelectedItem);
-//		}
+	//	private void Control_ItemSelected (object sender, SelectedItemChangedEventArgs e)
+	//	{
+	//		ItemSelected?.Invoke ((T)e.SelectedItem);
+	//	}
 
-//		class ListViewCellTemplate<T> : Template {
-//			public Func<T, FView> CellFor { get; set; }
-//			protected override DataTemplate OnSelectTemplate (object item, BindableObject container)
-//			{
-//				return new DataTemplate (() => {
-//					return new ViewCell {
-//						View = CellFor.Invoke ((T)item)
-//					};
-//				});
-//			}
-//		}
-//	}
+	//	class ListViewCellTemplate<T> : Template {
+	//		public Func<T, FView> CellFor { get; set; }
+	//		protected override DataTemplate OnSelectTemplate (object item, BindableObject container)
+	//		{
+	//			return new DataTemplate (() => {
+	//				return new ViewCell {
+	//					View = CellFor.Invoke ((T)item)
+	//				};
+	//			});
+	//		}
+	//	}
+	//}
 
-//	//Going to add a custom renderer, so it doesnt use forms horrible abstraction over tables views
-//	//public class GroupedListView<T> : View<FControlType> {
+	//Going to add a custom renderer, so it doesnt use forms horrible abstraction over tables views
+	//public class GroupedListView<T> : View<FControlType> {
 
-//	//	public Func<int> NumberOfSections { get; set; }
-//	//	public Func<int,int> RowsInSections { get; set; }
-//	//	public Func<(int section, int row), T> ObjectForRow { get; set; }
-//	//	public Func<T, FView> ViewFor { get; set; }
+	//	public Func<int> NumberOfSections { get; set; }
+	//	public Func<int,int> RowsInSections { get; set; }
+	//	public Func<(int section, int row), T> ObjectForRow { get; set; }
+	//	public Func<T, FView> ViewFor { get; set; }
 
-//	//	public Action<T> ItemSelected { get; set; }
+	//	public Action<T> ItemSelected { get; set; }
 
-//	//	protected override object CreateFormsView ()
-//	//	{
-//	//		var control = (FControlType)base.CreateFormsView ();
-//	//		control.HasUnevenRows = true;
-//	//		if (ViewFor != null)
-//	//			control.ItemTemplate = new ListViewCellTemplate<T> { CellFor = ViewFor };
-//	//		if (ItemSelected != null)
-//	//			control.ItemSelected += Control_ItemSelected;
-//	//		return control;
-//	//	}
+	//	protected override object CreateFormsView ()
+	//	{
+	//		var control = (FControlType)base.CreateFormsView ();
+	//		control.HasUnevenRows = true;
+	//		if (ViewFor != null)
+	//			control.ItemTemplate = new ListViewCellTemplate<T> { CellFor = ViewFor };
+	//		if (ItemSelected != null)
+	//			control.ItemSelected += Control_ItemSelected;
+	//		return control;
+	//	}
 
-//	//	private void Control_ItemSelected (object sender, SelectedItemChangedEventArgs e)
-//	//	{
-//	//		ItemSelected?.Invoke ((T)e.SelectedItem);
-//	//	}
+	//	private void Control_ItemSelected (object sender, SelectedItemChangedEventArgs e)
+	//	{
+	//		ItemSelected?.Invoke ((T)e.SelectedItem);
+	//	}
 
-//	//	class ListViewCellTemplate<T> : Template {
-//	//		public Func<T, FView> CellFor { get; set; }
-//	//		protected override DataTemplate OnSelectTemplate (object item, BindableObject container)
-//	//		{
-//	//			return new DataTemplate (() => {
-//	//				return new ViewCell {
-//	//					View = CellFor.Invoke ((T)item)
-//	//				};
-//	//			});
-//	//		}
-//	//	}
-//	//}
+	//	class ListViewCellTemplate<T> : Template {
+	//		public Func<T, FView> CellFor { get; set; }
+	//		protected override DataTemplate OnSelectTemplate (object item, BindableObject container)
+	//		{
+	//			return new DataTemplate (() => {
+	//				return new ViewCell {
+	//					View = CellFor.Invoke ((T)item)
+	//				};
+	//			});
+	//		}
+	//	}
+	//}
 
-//}
+}
