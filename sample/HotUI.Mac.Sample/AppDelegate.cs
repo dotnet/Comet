76f17b285e6baeed4fd7840899e28e045d00f0be
Jon Lipsky <jon@elevenworks.com>
2019-06-24 16:31:35 -0700

    Added mac sample and hacked it enough to work.

---
+++ sample/HotUI.Mac.Sample/AppDelegate.cs
@@ -0,0 +1,33 @@
+﻿using AppKit;
+using CoreGraphics;
+using Foundation;
+using HotUI.Samples;
+using HotUI.Mac.Extensions;
+
+namespace HotUI.Mac.Sample
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : NSApplicationDelegate
+    {
+        private NSWindow _window;
+
+        public AppDelegate()
+        {
+        }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            // Insert code here to initialize your application
+
+            /*_window = new NSWindow(new CGRect(10, 10, 800, 600), NSWindowStyle.UnifiedTitleAndToolbar, NSBackingStore.Buffered, false);
+            _window.ContentViewController = new MainPage().ToViewController();
+            _window.IsVisible = true;
+            _window.MakeKeyAndOrderFront(this);*/
+        }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
+    }
+}
---
+++ src/HotUI.Mac/Controls/StackView.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using AppKit;
+using CoreGraphics;
+
+namespace HotUI.Mac.Controls
+{
+    public class StackView : NSView
+    {
+        private bool _needsLayout;
+
+        public StackView(CGRect rect) : base(rect)
+        {
+        }
+
+        public StackView()
+        {
+        }
+
+        public override bool IsFlipped => true;
+
+        protected void LayoutSubviews()
+        {
+            var position = new CGPoint();
+
+            foreach (var view in Subviews)
+            {
+                var bounds = view.Bounds;
+                view.Frame = new CGRect(position, bounds.Size);
+                position.Y += bounds.Height;
+            }
+
+            _needsLayout = false;
+        }
+
+        public override void WillRemoveSubview(NSView subview)
+        {
+            base.WillRemoveSubview(subview);
+            _needsLayout = true;
+        }
+
+        public override void DidAddSubview(NSView subview)
+        {
+            base.DidAddSubview(subview);
+            _needsLayout = true;
+        }
+
+        public override CGRect Frame
+        {
+            get => base.Frame;
+            set
+            {
+                base.Frame = value;
+                LayoutSubviews();
+                _needsLayout = false;
+            }
+        }
+
+        public override void ViewDidMoveToSuperview()
+        {
+            if (_needsLayout)
+            {
+                LayoutSubviews();
+                _needsLayout = false;
+            }
+
+            base.ViewDidMoveToSuperview();
+        }
+    }
+}
