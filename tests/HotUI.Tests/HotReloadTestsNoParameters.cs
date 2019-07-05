32fa0ec28d66f5a7c5821dc987c51acf7286dae9
James Clancey <james.clancey@gmail.com>
2019-07-05 15:10:43 -0800

    Added in Missing HotReload

---
+++ tests/HotUI.Tests/HotReloadTestsNoParameters.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xunit;
+
+namespace HotUI.Tests {
+	public class HotReloadTests {
+
+		public class MyOrgView : View {
+			public const string TextValue = "Hello!";
+			public MyOrgView ()
+			{
+				this.Body = () => new Text (TextValue);
+			}
+		}
+		public class MyNewView : View {
+			public const string TextValue = "Goodbye!";
+			public MyNewView ()
+			{
+				this.Body = () => new Text (TextValue);
+			}
+		}
+
+		public HotReloadTests ()
+		{
+			HotReloadHelper.IsEnabled = true;
+		}
+
+		[Fact]
+		public void HotReloadRegisterReplacedViewReplacesView ()
+		{
+			var orgView = new MyOrgView ();
+			var orgText = orgView.GetView () as Text;
+			Assert.Equal (MyOrgView.TextValue, orgText.Value);
+
+			HotReloadHelper.RegisterReplacedView (typeof (MyOrgView).FullName, typeof (MyNewView));
+			var newText = orgView.GetView () as Text;
+
+			Assert.Equal (MyNewView.TextValue, newText.Value);
+
+		}
+	}
+}
