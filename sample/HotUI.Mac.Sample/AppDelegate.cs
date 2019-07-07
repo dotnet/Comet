b9790d8ae968d750e12a26cf2960b7e36773298a
James Clancey <james.clancey@gmail.com>
2019-07-06 18:31:58 -0800

    Samples now use HotUI.Reload

--- sample/HotUI.Mac.Sample/AppDelegate.cs
+++ sample/HotUI.Mac.Sample/AppDelegate.cs
@@ -17,18 +17,18 @@ namespace HotUI.Mac.Sample
 
         public override void DidFinishLaunching(NSNotification notification)
         {
-            // Insert code here to initialize your application
+			// Insert code here to initialize your application
 
-            /*_window = new NSWindow(new CGRect(10, 10, 800, 600), NSWindowStyle.UnifiedTitleAndToolbar, NSBackingStore.Buffered, false);
+			/*_window = new NSWindow(new CGRect(10, 10, 800, 600), NSWindowStyle.UnifiedTitleAndToolbar, NSBackingStore.Buffered, false);
             _window.ContentViewController = new MainPage().ToViewController();
             _window.IsVisible = true;
             _window.MakeKeyAndOrderFront(this);*/
-//#if DEBUG
-//			HotUI.Reload.Init ();
-//#endif
+#if DEBUG
+			HotUI.Reload.Init ();
+#endif
 		}
 
-        public override void WillTerminate(NSNotification notification)
+		public override void WillTerminate(NSNotification notification)
         {
             // Insert code here to tear down your application
         }
