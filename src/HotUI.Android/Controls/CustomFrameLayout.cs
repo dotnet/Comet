c52fd8261e31b6a93784750ba8321b7af19eb1c8
James Clancey <james.clancey@gmail.com>
2019-07-16 12:38:28 -0800

    Android navigation now works

---
+++ src/HotUI.Android/Controls/CustomFrameLayout.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Android.Content;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using LP = Android.Views.ViewGroup.LayoutParams;
+namespace HotUI.Android.Controls
+{
+    public class CustomFrameLayout : FrameLayout
+    {
+        public CustomFrameLayout(Context context) : base(context)
+        {
+            LayoutParameters = new LP(LP.MatchParent, LP.MatchParent);
+            Id = global::Android.Views.View.GenerateViewId();
+        }
+
+        public CustomFrameLayout(Context context, IAttributeSet attrs) : base(context, attrs)
+        {
+        }
+
+        public CustomFrameLayout(Context context, IAttributeSet attrs, int defStyleAttr) : base(context, attrs, defStyleAttr)
+        {
+        }
+
+        public CustomFrameLayout(Context context, IAttributeSet attrs, int defStyleAttr, int defStyleRes) : base(context, attrs, defStyleAttr, defStyleRes)
+        {
+        }
+
+        protected CustomFrameLayout(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+        }
+
+        public override WindowInsets OnApplyWindowInsets(WindowInsets insets)
+        {
+            var leftPadding = PaddingLeft;
+
+            var result = base.OnApplyWindowInsets(insets);
+
+            SetPadding(leftPadding, PaddingTop, PaddingRight, PaddingBottom);
+
+            return result;
+        }
+    }
+}
