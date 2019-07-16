7397c0f72124d08f0444b6feb31f8de07cc93105
James Clancey <james.clancey@gmail.com>
2019-07-15 17:36:46 -0800

    Fixed the Slider sample so it doesn't use global binding

--- sample/HotUI.Samples/SliderSample1.cs
+++ sample/HotUI.Samples/SliderSample1.cs
@@ -28,8 +28,8 @@ namespace HotUI.Samples
                 //new Slider(value: () => 12f, from: -100, through: 100, by: 0.1f),
                 //new Slider(value: new Binding<float>( getValue: () => 12f, setValue:null), from: -100, through: 100, by: 0.1f),
                 new Slider(value: celsius, from: -100, through: 100, by: 0.1f),
-                new Text($"{celsius.Value} Celsius"),
-                new Text($"{celsius.Value * 9 / 5 + 32} Fahrenheit"),
+                new Text(()=>$"{celsius.Value} Celsius"),
+                new Text(()=>$"{celsius.Value * 9 / 5 + 32} Fahrenheit"),
             };
 
     }
