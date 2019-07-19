9f0b372800cd4f10f62f3b6b604724fecb3e6800
James Clancey <james.clancey@gmail.com>
2019-07-19 15:08:18 -0800

    added color picker to Skia 3

--- sample/HotUI.Samples/Skia/SkiaSample3.cs
+++ sample/HotUI.Samples/Skia/SkiaSample3.cs
@@ -22,9 +22,26 @@ namespace HotUI.Samples.Skia
                 },
                 new HStack()
                 {
-                    new Text("Stroke Color:"),
-                    new TextField(_strokeColor)
+                    new Text("Stroke Color!:"),
+                    new TextField(_strokeColor),
                 },
+                //new ScrollView{
+                    new HStack
+                    {
+                        new Button("Black", () =>
+                        {
+                            _strokeColor.Value = Color.Black.ToHexString();
+                        }),
+                        new Button("Blue", () =>
+                        {
+                            _strokeColor.Value = Color.Blue.ToHexString();
+                        }),
+                        new Button("Red", () =>
+                        {
+                            _strokeColor.Value = Color.Red.ToHexString();
+                        })
+                    },
+                //},
                 new BindableFingerPaint(
                     strokeSize:_strokeSize, 
                     strokeColor:_strokeColor).ToView().Frame(height:400)
