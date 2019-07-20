f97104163927ca34e710c82358216fb894ca00ec
Sweeky Satpathy <swsat@microsoft.com>
2019-07-19 18:15:51 -0700

    Revert "Revert "Skia now works on WPF""
    This reverts commit c858c592ac56e42cbad475624c576728339e351a.

---
+++ src/HotUI.Skia.WPF/DrawableControlHandler.cs
@@ -0,0 +1,42 @@
+﻿using HotUI.WPF.Handlers;
+
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable MemberCanBePrivate.Global
+
+namespace HotUI.Skia.WPF
+{
+    public class DrawableControlHandler : AbstractControlHandler<DrawableControl, WPFDrawableControl>
+    {
+        protected override WPFDrawableControl CreateView()
+        {
+            return new WPFDrawableControl();
+        }
+
+        protected override void DisposeView(WPFDrawableControl nativeView)
+        {
+            
+        }
+
+        public override void SetView(View view)
+        {
+            base.SetView(view);
+
+            SetMapper(VirtualView.ControlDelegate.Mapper);
+            TypedNativeView.ControlDelegate = VirtualView.ControlDelegate;
+            VirtualView.ControlDelegate.Mapper?.UpdateProperties(this, VirtualView);
+        }
+
+        public override void Remove(View view)
+        {
+            TypedNativeView.ControlDelegate = null;
+            SetMapper(null);
+            
+            base.Remove(view);
+        }
+
+        public override SizeF Measure(SizeF availableSize)
+        {
+            return VirtualView?.ControlDelegate?.Measure(availableSize) ?? availableSize;
+        }
+    }
+}
