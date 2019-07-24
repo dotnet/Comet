cd6170b1c47c28f6faa41ecc5a34da34ee75e558
Alexey Strakh <alexey.strakh@microsoft.com>
2019-07-23 18:12:25 -0700

    add test animation layout

--- sample/HotUI.Samples/AnimationSample.cs
+++ sample/HotUI.Samples/AnimationSample.cs
@@ -8,16 +8,21 @@
         }
 
         View Build() =>
-            new Text("Text to Animate!")
-                .Background(Color.Orange)
-                .Animate(new Animation
-                {
-                    Duration = 2000,
-                    Delay = 500,
-                    Options = AnimationOptions.CurveEaseOut | AnimationOptions.Repeat,
-                    TranslateTo = new PointF(10, 10),
-                    RotateTo = 30,
-                    ScaleTo = new PointF(2f, 2f),
-                });
+            new VStack
+            {
+                new Text("Regular Text Behind..."),
+                new Text("Text to Animate!")
+                    .Background(Color.Orange)
+                    .Animate(new Animation
+                    {
+                        Duration = 2000,
+                        Delay = 500,
+                        Options = AnimationOptions.CurveEaseOut | AnimationOptions.Repeat,
+                        TranslateTo = new PointF(100, 50),
+                        RotateTo = 30,
+                        ScaleTo = new PointF(2f, 2f),
+                    }),
+                new Text("Regular Text Above..."),
+            };
     }
 }
