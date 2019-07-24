b6578b3e37e69f4313fdcb4d0c42f0bc09775bc6
Taylor Southwick <tasou@microsoft.com>
2019-07-23 17:15:29 -0700

    Add initial blazor support

---
+++ src/HotUI.Blazor/Components/BView.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components.RenderTree;
+
+namespace HotUI.Blazor.Components
+{
+    public class BView : HotUIComponentBase
+    {
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+
+            builder.AddView(0, View);
+        }
+
+        public View View { get; set; }
+    }
+}
