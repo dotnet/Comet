fc6b10eaef387668eca65ef509cbfbe05cd7c28b
Jon Lipsky <jon@elevenworks.com>
2019-08-06 23:04:47 -0500

    Removed dead code.

--- src/HotUI.Skia/DrawableControl.cs
+++ src/HotUI.Skia/DrawableControl.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using HotUI.Skia;
+﻿using HotUI.Skia;
 
 namespace HotUI 
 {
@@ -18,30 +14,10 @@ namespace HotUI
 		
 		internal State GetState() => State;
 
-		public void SetStateValue<T> (ref T currentValue, T newValue, [CallerMemberName] string propertyName = "")
-		{
-			State.SetValue (ref currentValue, newValue, this , propertyName);
-		}
-		
-
 		protected override void ViewPropertyChanged(string property, object value)
 		{
 			ControlDelegate?.ViewPropertyChanged(property, value);
 			base.ViewPropertyChanged(property, value);
 		}
-	}
-	
-	public class BindingDefinition
-	{
-		public BindingDefinition(Binding binding, string propertyName, Action<object> updater)
-		{
-			Binding = binding;
-			PropertyName = propertyName;
-			Updater = updater;
-		}
-
-		public Binding Binding { get; }
-		public string PropertyName { get; }
-		public Action<object> Updater {  get; }
 	}
 }
