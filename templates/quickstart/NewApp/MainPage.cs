30c9e552ff8621ac4d48e522b7181d4287baf49f
David Ortinau <david.ortinau@microsoft.com>
2019-08-23 17:22:07 -0400

    cleaning up compared to Samples project settings

--- templates/quickstart/NewApp/MainPage.cs
+++ templates/quickstart/NewApp/MainPage.cs
@@ -11,6 +11,7 @@ namespace NewApp
         View body() => new VStack
         {
             new Text(() => $"Value: {count.Value}")
+                .Color(Color.Black)
                 .FontSize(32),
             new Button("Increment", () => count.Value ++ )
                 .Frame(width:320, height:44)
