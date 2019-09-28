0d291c713a46e63d8ea0ac7825d13d598db687ee
David Ortinau <david.ortinau@microsoft.com>
2019-09-28 11:56:09 +0200

    added android head project and update branding assets

---
+++ templates/quickstart/NewApp.Android/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace NewApp.Droid
+{
+    [Activity(Label = "NewApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            #if (kind == "shell")
+            global::Xamarin.Forms.Forms.SetFlags("CollectionView_Experimental");
+            #endif
+            #if (IncludeXamarinEssentials)
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            #endif
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+        #if(IncludeXamarinEssentials)
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        #endif
+    }
+}
