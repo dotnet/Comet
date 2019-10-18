533664f7d7087bd7cd4c1a931073e6ab42ece8d1
James Clancey <james.clancey@gmail.com>
2019-10-17 17:54:20 -0800

    Started on Material Design

---
+++ src/Comet/Styles/Material/MaterialDesign.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Comet.Styles
+{
+    public  static partial class MaterialDesign
+    {
+        public static void Apply()
+        {
+            SetColorPalette(PrimaryColorPalette, SecondaryColorPalette);
+        }
+        public static T Apply<T>(this T view ) where T :View
+        {
+            return view;
+        }
+        public static void SetColorPalette(ColorPalette primary, ColorPalette secondary)
+        {
+
+        }
+
+        public static ColorPalette PrimaryColorPalette
+        {
+            get => View.GetGlobalEnvironment<ColorPalette>(nameof(PrimaryColorPalette)) ?? ColorPalettes.Blue;
+            set => View.SetGlobalEnvironment(nameof(PrimaryColorPalette), value);
+        }
+        public static ColorPalette SecondaryColorPalette
+        {
+            get => View.GetGlobalEnvironment<ColorPalette>(nameof(SecondaryColorPalette)) ?? ColorPalettes.Blue;
+            set => View.SetGlobalEnvironment(nameof(SecondaryColorPalette), value);
+        }
+
+
+    }
+}
