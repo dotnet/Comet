197fce187f5ce27fd7734a1e0237126e1e6a2a3c
James Clancey <james.clancey@gmail.com>
2019-11-19 15:24:04 -0900

    Android navigation now re-uses it's native navigation view

--- src/Comet.Android/Handlers/NavigationViewHandler.cs
+++ src/Comet.Android/Handlers/NavigationViewHandler.cs
@@ -7,19 +7,29 @@ namespace Comet.Android.Handlers
 {
     public class NavigationViewHandler : AbstractHandler<NavigationView, CUINavigationView>
     {
+        CUINavigationView navigationView;
         protected override CUINavigationView CreateView(Context context)
         {
-            var view = new CUINavigationView(context);
+            navigationView ??= new CUINavigationView(context);
 
             if (VirtualView != null)
             {
-                view.SetRoot(VirtualView?.Content);
-                VirtualView.PerformNavigate = view.NavigateTo;
+                navigationView.SetRoot(VirtualView?.Content);
+                VirtualView.PerformNavigate = navigationView.NavigateTo;
             }
 
-            return view;
+            return navigationView;
         }
-
+        public override void SetView(View view)
+        {
+            var nav = view as NavigationView;
+            if (navigationView != null)
+            {
+                navigationView.SetRoot(nav.Content);
+                nav.PerformNavigate = navigationView.NavigateTo;
+            }
+            base.SetView(view);
+        }
         public override void Remove(View view)
         {
             if (VirtualView != null)
