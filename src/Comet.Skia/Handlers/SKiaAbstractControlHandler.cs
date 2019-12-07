0382c3f95155c59c7005d07f5d0a3166b22ee2ec
James Clancey <james.clancey@gmail.com>
2019-12-06 22:25:40 -0900

    Code Formatting

--- src/Comet.Skia/Handlers/SKiaAbstractControlHandler.cs
+++ src/Comet.Skia/Handlers/SKiaAbstractControlHandler.cs
@@ -5,12 +5,12 @@ using SkiaSharp;
 namespace Comet.Skia
 {
 	public abstract class SKiaAbstractControlHandler<TVirtualView> : SkiaControl, IViewHandler
-        where TVirtualView : View
-    {
+		where TVirtualView : View
+	{
 
-        PropertyMapper<TVirtualView> mapper;
+		PropertyMapper<TVirtualView> mapper;
 		protected DrawMapper<TVirtualView> drawMapper;
-		
+
 
 		protected SKiaAbstractControlHandler()
 		{
@@ -24,69 +24,69 @@ namespace Comet.Skia
 			this.mapper = mapper ?? new PropertyMapper<TVirtualView>(SkiaControl.Mapper);
 		}
 
-        public static string[] DefaultLayerDrawingOrder = new[]
-        {
-            SkiaEnvironmentKeys.Clip,
-            SkiaEnvironmentKeys.Background,
-            SkiaEnvironmentKeys.Border,
-            SkiaEnvironmentKeys.Text,
-            SkiaEnvironmentKeys.Overlay,
-        };
-        protected virtual string[] LayerDrawingOrder() => DefaultLayerDrawingOrder;
-
-
-        public override void Draw(SKCanvas canvas, RectangleF dirtyRect)
+		public static string[] DefaultLayerDrawingOrder = new[]
 		{
-            canvas.Clear(Color.Transparent.ToSKColor());
-            if (TypedVirtualView == null || drawMapper == null)
-                return;
-            canvas.Save();
-            var layers = LayerDrawingOrder();
-            var padding = this.GetPadding();
+			SkiaEnvironmentKeys.Clip,
+			SkiaEnvironmentKeys.Background,
+			SkiaEnvironmentKeys.Border,
+			SkiaEnvironmentKeys.Text,
+			SkiaEnvironmentKeys.Overlay,
+		};
+		protected virtual string[] LayerDrawingOrder() => DefaultLayerDrawingOrder;
+
+
+		public override void Draw(SKCanvas canvas, RectangleF dirtyRect)
+		{
+			canvas.Clear(Color.Transparent.ToSKColor());
+			if (TypedVirtualView == null || drawMapper == null)
+				return;
+			canvas.Save();
+			var layers = LayerDrawingOrder();
+			var padding = this.GetPadding();
 			var rect = dirtyRect.ApplyPadding(padding);
-			foreach(var layer in layers)
-            {
-                drawMapper.DrawLayer(canvas, rect, this, TypedVirtualView, layer);
-            }
+			foreach (var layer in layers)
+			{
+				drawMapper.DrawLayer(canvas, rect, this, TypedVirtualView, layer);
+			}
 
-            var clipShape = VirtualView?.GetClipShape() ?? VirtualView?.GetBorder();
-            if (clipShape != null)
-                canvas.ClipPath(clipShape.PathForBounds(rect).ToSKPath());
+			var clipShape = VirtualView?.GetClipShape() ?? VirtualView?.GetBorder();
+			if (clipShape != null)
+				canvas.ClipPath(clipShape.PathForBounds(rect).ToSKPath());
 
-            drawMapper.DrawLayer(canvas, rect, this, TypedVirtualView, SkiaEnvironmentKeys.Border);
+			drawMapper.DrawLayer(canvas, rect, this, TypedVirtualView, SkiaEnvironmentKeys.Border);
 
-            canvas.Restore();
-        }
+			canvas.Restore();
+		}
 
 		public TVirtualView TypedVirtualView { get; private set; }
 
-        public object NativeView => throw new NotImplementedException();
+		public object NativeView => throw new NotImplementedException();
 
-        public bool HasContainer { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		public bool HasContainer { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
-        public override void SetView(View view)
+		public override void SetView(View view)
 		{
 			TypedVirtualView = view as TVirtualView;
 			mapper?.UpdateProperties(this, TypedVirtualView);
 			base.SetView(view);
 		}
 
-        public void UpdateValue(string property, object value)
-        {
+		public void UpdateValue(string property, object value)
+		{
 			mapper?.UpdateProperty(this, TypedVirtualView, property);
 		}
-        public override void ViewPropertyChanged(string property, object value)
-        {
-            Invalidate();
-        }
-        public void Remove(View view)
-        {
-            throw new NotImplementedException();
-        }
+		public override void ViewPropertyChanged(string property, object value)
+		{
+			Invalidate();
+		}
+		public void Remove(View view)
+		{
+			throw new NotImplementedException();
+		}
 
-        public void SetFrame(RectangleF frame)
-        {
-            throw new NotImplementedException();
-        }
-    }
+		public void SetFrame(RectangleF frame)
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
