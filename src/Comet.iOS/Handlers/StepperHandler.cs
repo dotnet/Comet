9a1f2a3e28764b25307a8c15603d60f9fbade450
Libin Joseph <libin.joseph@outlook.com>
2019-12-11 22:17:33 +1100

    Intial commit for Stepper control
    This commit adds the stepper control for ios, and a sample page that demonstrates how to use it

--- src/Comet.iOS/Handlers/StepperHandler.cs
+++ src/Comet.iOS/Handlers/StepperHandler.cs
@@ -12,12 +12,18 @@ namespace Comet.iOS.Handlers
     {
         public static readonly PropertyMapper<Stepper> Mapper = new PropertyMapper<Stepper>(ViewHandler.Mapper)
         {
-            [nameof(Stepper.Value)] = MapValueProperty
+            [nameof(Stepper.Value)] = MapValueProperty,
+            [nameof(Stepper.Maximum)] = MapMaximumProperty,
+            [nameof(Stepper.Minimum)] = MapMinimumProperty,
+            [nameof(Stepper.Increment)] = MapIncremntProperty,
         };
+
+        
         public StepperHandler() :base(Mapper)
         {
 
         }
+
         protected override UIStepper CreateView()
         {
             var uiStepper = new UIStepper();
@@ -27,7 +33,7 @@ namespace Comet.iOS.Handlers
 
         private void HandleValueChanged(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            VirtualView?.OnValueChanged.Invoke(TypedNativeView.Value);
         }
 
         protected override void DisposeView(UIStepper nativeView)
@@ -40,5 +46,24 @@ namespace Comet.iOS.Handlers
             var nativeView = (UIStepper)viewHandler.NativeView;
             nativeView.Value = virtualView.Value.CurrentValue;
         }
+
+        private static void MapIncremntProperty(IViewHandler viewHandler, Stepper virtualView)
+        {
+            var nativeView = (UIStepper)viewHandler.NativeView;
+            nativeView.StepValue = virtualView.Increment.CurrentValue;
+        }
+
+        private static void MapMinimumProperty(IViewHandler viewHandler, Stepper virtualView)
+        {
+            var nativeView = (UIStepper)viewHandler.NativeView;
+            nativeView.MinimumValue = virtualView.Minimum.CurrentValue;
+        }
+
+        private static void MapMaximumProperty(IViewHandler viewHandler, Stepper virtualView)
+        {
+            var nativeView = (UIStepper)viewHandler.NativeView;
+            nativeView.MaximumValue = virtualView.Maximum.CurrentValue;
+        }
+
     }
 }
--- src/Comet/Controls/Stepper.cs
+++ src/Comet/Controls/Stepper.cs
@@ -6,9 +6,19 @@ namespace Comet
 {
     public class Stepper : View
     {
-        public Stepper(Binding<double> value = null)
+        public Stepper(Binding<double> value = null,
+            Binding<double> maximumValue = null,
+            Binding<double> minimumValue = null,
+            Binding<double> increment = null,
+            Action<double> onValueChanged = null)
         {
+            if (minimumValue >= maximumValue)
+                throw new ArgumentOutOfRangeException(nameof(minimumValue),"Minimum value is greater than the maximum value");
             Value = value;
+            Maximum = maximumValue;
+            Minimum = minimumValue;
+            Increment = increment;
+            OnValueChanged = new MulticastAction<double>(value, onValueChanged);
         }
 
         Binding<double> _value;
@@ -17,5 +27,28 @@ namespace Comet
             get => _value;
             private set => this.SetBindingValue(ref _value, value);
         }
+
+        Binding<double> _maximum;
+        public Binding<double> Maximum
+        {
+            get => _maximum;
+            private set => this.SetBindingValue(ref _maximum, value);
+        }
+
+        Binding<double> _minimum;
+        public Binding<double> Minimum
+        {
+            get => _minimum;
+            private set => this.SetBindingValue(ref _minimum, value);
+        }
+
+        Binding<double> _increment;
+        public Binding<double> Increment
+        {
+            get => _increment;
+            private set => this.SetBindingValue(ref _increment, value);
+        }
+
+        public Action<double> OnValueChanged { get; private set; }
     }
 }
