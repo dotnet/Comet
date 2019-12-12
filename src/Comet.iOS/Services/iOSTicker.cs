1fed350b2b4c9693c0a4ccbd07558fdd1190b01b
James Clancey <james.clancey@gmail.com>
2019-12-11 20:04:12 -0900

    code formatting

--- src/Comet.iOS/Services/iOSTicker.cs
+++ src/Comet.iOS/Services/iOSTicker.cs
@@ -4,29 +4,29 @@ using Foundation;
 
 namespace Comet.iOS
 {
-    public class iOSTicker : Ticker
-    {
-        public iOSTicker()
-        {
-        }
-        CADisplayLink link;
+	public class iOSTicker : Ticker
+	{
+		public iOSTicker()
+		{
+		}
+		CADisplayLink link;
 
-        public override bool IsRunning => link != null;
-        public override void Start()
-        {
-            if (link != null)
-                return;
-            link = CADisplayLink.Create(() => Fire?.Invoke());
-            link.AddToRunLoop(NSRunLoop.Current, NSRunLoop.NSRunLoopCommonModes);
+		public override bool IsRunning => link != null;
+		public override void Start()
+		{
+			if (link != null)
+				return;
+			link = CADisplayLink.Create(() => Fire?.Invoke());
+			link.AddToRunLoop(NSRunLoop.Current, NSRunLoop.NSRunLoopCommonModes);
 
-        }
-        public override void Stop()
-        {
-            if (link == null)
-                return;
-            link?.RemoveFromRunLoop(NSRunLoop.Current, NSRunLoop.NSRunLoopCommonModes);
-            link?.Dispose();
-            link = null;
-        }
-    }
+		}
+		public override void Stop()
+		{
+			if (link == null)
+				return;
+			link?.RemoveFromRunLoop(NSRunLoop.Current, NSRunLoop.NSRunLoopCommonModes);
+			link?.Dispose();
+			link = null;
+		}
+	}
 }
