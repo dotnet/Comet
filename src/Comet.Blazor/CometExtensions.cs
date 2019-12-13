3c85e176ea5d63717f031c18953209d82429534d
James Clancey <james.clancey@gmail.com>
2019-12-13 10:04:23 -0900

    Blazor Code formatting

--- src/Comet.Blazor/CometExtensions.cs
+++ src/Comet.Blazor/CometExtensions.cs
@@ -3,32 +3,30 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Comet.Blazor
 {
-    public static class CometExtensions
-    {
-        public static void AddComet(this IServiceCollection services)
-        {
-            services.AddScoped<CanvasWriter>();
-            services.AddImages();
-        }
+	public static class CometExtensions
+	{
+		public static void AddComet(this IServiceCollection services)
+		{
+			services.AddScoped<CanvasWriter>();
+			services.AddImages();
+		}
 
-        public static void UseComet(this IApplicationBuilder app)
-        {
-            UI.Init();
+		public static void UseComet(this IApplicationBuilder app)
+		{
+			UI.Init();
 
-            app.UseImages();
-            app.Map("/_comet/comet.js", app2 =>
-            {
-                app2.Run(async ctx =>
-                {
-                    using (var stream = typeof(CometExtensions).Assembly.GetManifestResourceStream(typeof(CometExtensions), "Scripts.comet.js"))
-                    {
-                        ctx.Response.StatusCode = 200;
-                        ctx.Response.ContentType = "application/javascript";
+			app.UseImages();
+			app.Map("/_comet/comet.js", app2 => {
+				app2.Run(async ctx => {
+					using (var stream = typeof(CometExtensions).Assembly.GetManifestResourceStream(typeof(CometExtensions), "Scripts.comet.js"))
+					{
+						ctx.Response.StatusCode = 200;
+						ctx.Response.ContentType = "application/javascript";
 
-                        await stream.CopyToAsync(ctx.Response.Body);
-                    }
-                });
-            });
-        }
-    }
+						await stream.CopyToAsync(ctx.Response.Body);
+					}
+				});
+			});
+		}
+	}
 }
