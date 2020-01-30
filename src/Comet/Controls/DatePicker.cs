5c3b8e68089b7dac30ad0c22897a7d46827ccca1
Libin Joseph <libin.joseph@outlook.com>
2020-01-31 06:23:36 +1100

    Date picker control (#167)
    * merged with upstream
    
    * merged with upstream
    
    * added the datepicker control for ios
    
    * added a sample for date picker
    
    * made the date format consistent when the control is first loaded and after the value is changed
    
    * have changed from space to tab
    
    * changed all spaced to tabs

---
+++ src/Comet/Controls/DatePicker.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace Comet
+{
+	public class DatePicker : View
+	{
+		public DatePicker(Binding<DateTime> date = null,
+			Binding < DateTime> maximumDate = null,
+			Binding<DateTime> minimumDate = null,
+			Binding<string> format = null,
+			Action<DateTime> onDateChnaged = null)
+		{
+			if (minimumDate?.CurrentValue >= maximumDate?.CurrentValue)
+				throw new ArgumentOutOfRangeException(nameof(minimumDate), "Minimum date is greater than the maximum date");
+			Date = date;
+			MaximumDate = maximumDate;
+			MinimumDate = minimumDate;
+			Format = format;
+			OnDateChanged = new MulticastAction<DateTime>(date, onDateChnaged);
+		}
+
+
+		Binding<DateTime> _maximumDate;
+		public Binding<DateTime> MaximumDate
+		{
+			get => _maximumDate;
+			private set => this.SetBindingValue(ref _maximumDate, value);
+		}
+
+		Binding<DateTime> _minimumDate;
+		public Binding<DateTime> MinimumDate
+		{
+			get => _minimumDate;
+			private set => this.SetBindingValue(ref _minimumDate, value);
+		}
+
+		Binding<string> _format;
+		public Binding<string> Format
+		{
+			get => _format;
+			private set => this.SetBindingValue(ref _format, value);
+		}
+
+		Binding<DateTime> _date;
+		public Binding<DateTime> Date
+		{
+			get => _date;
+			private set => this.SetBindingValue(ref _date, value);
+		}
+
+		public Action<DateTime> OnDateChanged { get; private set; }
+	}
+}
