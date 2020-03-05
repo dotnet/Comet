9e836441510490fafe56ac9b3fa779003c94e2cb
James Clancey <james.clancey@gmail.com>
2020-03-05 13:11:07 -0900

    Wpf CometPage is now an IViewHandler

--- src/Comet.WPF/CometPage.xaml.cs
+++ src/Comet.WPF/CometPage.xaml.cs
@@ -1,9 +1,10 @@
-﻿using System.Windows.Controls;
+﻿using System.Drawing;
+using System.Windows.Controls;
 using WGrid = System.Windows.Controls.Grid;
 
 namespace Comet.WPF
 {
-	public sealed partial class CometPage : Page
+	public sealed partial class CometPage : Page, IViewHandler
 	{
 		private View _view;
 		private readonly Frame _frame;
@@ -16,21 +17,48 @@ namespace Comet.WPF
 			View = view;
 		}
 
+		CometContainerView CometView;
 		public View View
 		{
 			get => _view;
 			private set
 			{
 				_view = value;
-				Content = value?.ToEmbeddableView() ?? new WGrid();
+				Content = CometView = (CometContainerView)value?.ToEmbeddableView() ?? new CometContainerView();
 
 				if (_view?.BuiltView is NavigationView nav)
 				{
 					nav.SetPerformNavigate(toView => {
 						_frame.NavigationService.Navigate(new CometPage(_frame, toView));
 					});
-					nav.SetPerformPop(() => _frame.NavigationService.GoBack());				}
+					nav.SetPerformPop(() => _frame.NavigationService.GoBack());
+				}
+				if (_view.ViewHandler == null)
+					_view.ViewHandler = this;
 			}
+		}
+
+		public object NativeView => null;
+
+		public bool HasContainer { get; set; }
+
+		public void SetView(View view) => CometView.View = view;
+
+		public void UpdateValue(string property, object value)
+		{
+
+		}
+
+		public void Remove(View view)
+		{
+
+		}
+
+		public SizeF GetIntrinsicSize(SizeF availableSize) => availableSize;
+
+		public void SetFrame(RectangleF frame)
+		{
+
 		}
 	}
 }
