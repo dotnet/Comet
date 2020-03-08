a6c1399b185bbcd11a384e2f8090739ec75db69f
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2020-03-07 19:31:50 -0500

    [ios] Implement FontWeight and FontItalic and add sample page for them

--- sample/Comet.Samples/MainPage.cs
+++ sample/Comet.Samples/MainPage.cs
@@ -12,6 +12,7 @@ namespace Comet.Samples
 		{
 			new MenuItem("Ride the Comet",()=> new RideSample()),
 			new MenuItem("Text Styles",()=> new TextStylesSample()),
+			new MenuItem("Text Weight",()=> new TextWeightSample()),
 			new MenuItem("Skia Controls Sample",()=> new SkiaControlsSample()),
 			new MenuItem("Material Design",()=> new MaterialStylePicker()),
 			new MenuItem("AuditReportPage",()=> new AuditReportPage()),
