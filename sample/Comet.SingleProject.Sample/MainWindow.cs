08b2bce815b0a376e5ca638e1ee8ba30079b0c07
James Clancey <james.clancey@gmail.com>
2021-04-14 16:00:11 -0800

    started in net6 stuff again

---
+++ sample/Comet.SingleProject.Sample/MainWindow.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Maui;
+
+namespace Maui.Controls.Sample.SingleProject
+{
+	public class MainWindow : IWindow
+	{
+		public MainWindow()
+		{
+			Page = new MainPage();
+		}
+
+		public IPage Page { get; set; }
+
+		public IMauiContext MauiContext { get; set; }
+	}
+}
