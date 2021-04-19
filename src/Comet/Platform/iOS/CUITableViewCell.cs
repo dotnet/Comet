aadabbba4b4a0b0b5891934ac911867636181c9b
James Clancey <james.clancey@gmail.com>
2021-04-19 12:08:52 -0800

    Update CUITableViewCell.cs

--- src/Comet/Platform/iOS/CUITableViewCell.cs
+++ src/Comet/Platform/iOS/CUITableViewCell.cs
@@ -4,21 +4,33 @@ using UIKit;
 
 namespace Comet.iOS
 {
+	[Register("CUITableViewCell")]
 	public class CUITableViewCell : UITableViewCell
 	{
 		public static int _instanceCount;
 
 		CometView cometView;
-
-		public CUITableViewCell(IMauiContext context, UITableViewCellStyle style, string reuseIdentifier) : base(style, reuseIdentifier)
+		public CUITableViewCell(IntPtr ptr) : base(ptr)
 		{
 			ContentView.Tag = _instanceCount++;
+		}
 
+		public void SetFromContext(IMauiContext context)
+		{
+			if (cometView != null)
+				return;
+
+			ContentView.Tag = _instanceCount++;
 			cometView = new CometView(ContentView.Bounds, context);
 			cometView.AutoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight;
 
 			ContentView.AutosizesSubviews = true;
 			ContentView.AddSubview(cometView);
+		}
+
+		public CUITableViewCell(IMauiContext context, UITableViewCellStyle style, string reuseIdentifier) : base(style, reuseIdentifier)
+		{
+			SetFromContext(context);
 		}
 
 		public void SetView(View view)
