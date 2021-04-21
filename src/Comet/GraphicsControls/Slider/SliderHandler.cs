dbf65daaba35f1b739dd9211606ac80487a44fb4
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-04-21 18:06:52 +0200

    Add graphics Slider control

---
+++ src/Comet/GraphicsControls/Slider/SliderHandler.cs
@@ -0,0 +1,42 @@
+﻿using Comet.Handlers;
+using Microsoft.Maui;
+using Microsoft.Maui.Graphics;
+
+namespace Comet.GraphicsControls
+{
+	public abstract class SliderHandler : GraphicsControlHandler<SliderHandler, ISlider>
+	{
+		bool _isDragging;
+
+		protected SliderHandler(DrawMapper drawMapper, PropertyMapper mapper) : base(drawMapper, mapper)
+		{
+
+		}
+
+		public override bool StartInteraction(PointF[] points)
+		{
+			_isDragging = false;
+			return base.StartInteraction(points);
+		}
+
+		public override void DragInteraction(PointF[] points)
+		{
+			_isDragging = true;
+
+			if (!_isDragging)
+				VirtualView?.DragStarted();
+
+			base.DragInteraction(points);
+		}
+
+		public override void EndInteraction(PointF[] points, bool inside)
+		{
+			_isDragging = false;
+
+			if (_isDragging)
+				VirtualView?.DragCompleted();
+
+			base.EndInteraction(points, inside);
+		}
+	}
+}
