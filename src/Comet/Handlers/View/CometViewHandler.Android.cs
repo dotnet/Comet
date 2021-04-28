58bc11d485607f82bb7b0f52558ac46cb40c9365
James Clancey <james.clancey@gmail.com>
2021-04-28 09:23:47 -0800

    Working on DefaultView handler

---
+++ src/Comet/Handlers/View/CometViewHandler.Android.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.Maui;
+using Microsoft.Maui.Graphics;
+using Microsoft.Maui.Handlers;
+namespace Comet.Handlers
+{
+	public partial class CometViewHandler
+	{
+
+	}
+}
---
+++ src/Comet/Handlers/View/CometViewHandler.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace Comet.Handlers
+{
+	public partial class CometViewHandler
+	{
+		
+	}
+}
