423491d1c65400f927f5db1b527100e6b85de119
James Clancey <james.clancey@gmail.com>
2021-06-14 19:03:36 -0800

    Added Func<> versions to the color extension bindings

--- src/Comet/Helpers/ColorExtensions.cs
+++ src/Comet/Helpers/ColorExtensions.cs
@@ -24,15 +24,18 @@ namespace Comet
 			view.SetEnvironment(EnvironmentKeys.Colors.Color, color, false);
 			return view;
 		}
+		public static T Color<T>(this T view, Func<Color> color) where T : View => view.Color((Binding<Color>)color);
+
 		public static T Color<T>(this T view, Type type, Binding<Color> color) where T : View
 		{
 			view.SetEnvironment(type, EnvironmentKeys.Colors.Color, color, true);
 			return view;
 		}
+		public static T Color<T>(this T view, Type type, Func<Color> color) where T : View => view.Color(type: type, (Binding<Color>)color);
 
 		public static Color GetColor<T>(this T view, Color defaultColor = null, ControlState state = ControlState.Default) where T : View
 		{
-			var color = view.GetEnvironment<Color>(EnvironmentKeys.Colors.Color,state);
+			var color = view.GetEnvironment<Color>(EnvironmentKeys.Colors.Color, state);
 			return color ?? defaultColor;
 		}
 
@@ -66,6 +69,7 @@ namespace Comet
 			view.SetEnvironment(EnvironmentKeys.Colors.Background, color, cascades);
 			return view;
 		}
+		public static T Background<T>(this T view, Func<Color> color, bool cascades = false) where T : View => view.Background((Binding<Color>)color, cascades);
 
 		/// <summary>
 		/// Set the background color by hex value
@@ -79,6 +83,7 @@ namespace Comet
 			view.SetEnvironment(EnvironmentKeys.Colors.Background, colorHex, cascades);
 			return view;
 		}
+		public static T Background<T>(this T view, Func<string> colorHex, bool cascades = false) where T : View => view.Background((Binding<string>)colorHex, cascades);
 
 		/// <summary>
 		/// Set the background color
@@ -92,11 +97,13 @@ namespace Comet
 			view.SetEnvironment(type, EnvironmentKeys.Colors.Background, color, true);
 			return view;
 		}
+		public static T Background<T>(this T view, Type type, Func<Color> color) where T : View => view.Background(type, (Binding<Color>)color);
 		public static T Background<T>(this T view, Type type, Binding<Paint> paint) where T : View
 		{
 			view.SetEnvironment(type, EnvironmentKeys.Colors.Background, paint, true);
 			return view;
 		}
+		public static T Background<T>(this T view, Type type, Func<Paint> paint) where T : View => view.Background(type, (Binding<Paint>)paint);
 
 		public static Paint GetBackground(this View view, Type type, Paint defaultColor = null, ControlState state = ControlState.Default)
 		{
@@ -106,10 +113,10 @@ namespace Comet
 
 		public static Paint GetBackground(this View view, Paint defaultColor = null, ControlState state = ControlState.Default)
 		{
-			var color = view?.GetEnvironment<object>(EnvironmentKeys.Colors.Background,state);
+			var color = view?.GetEnvironment<object>(EnvironmentKeys.Colors.Background, state);
 			return color.ConvertToPaint() ?? defaultColor;
 		}
-		
+
 
 		public static Color GetNavigationBackgroundColor(this View view, Color defaultColor = null)
 		{
@@ -124,9 +131,10 @@ namespace Comet
 
 		public static T TrackColor<T>(this T view, Binding<Color> color, ControlState state = ControlState.Default) where T : Slider
 		{
-			view.SetEnvironment(EnvironmentKeys.Slider.TrackColor, color, cascades:false, state);
+			view.SetEnvironment(EnvironmentKeys.Slider.TrackColor, color, cascades: false, state);
 			return view;
 		}
+		public static T TrackColor<T>(this T view, Func<Color> color, ControlState state = ControlState.Default) where T : Slider => view.TrackColor((Binding<Color>)color, state);
 
 		public static Color GetTrackColor(this Slider view, Color defaultColor = null, ControlState state = ControlState.Default)
 		{
@@ -138,9 +146,10 @@ namespace Comet
 
 		public static T ProgressColor<T>(this T view, Binding<Color> color, ControlState state = ControlState.Default) where T : Slider
 		{
-			view.SetEnvironment(EnvironmentKeys.Slider.ProgressColor, color, cascades:false, state);
+			view.SetEnvironment(EnvironmentKeys.Slider.ProgressColor, color, cascades: false, state);
 			return view;
 		}
+		public static T ProgressColor<T>(this T view, Func<Color> color, ControlState state = ControlState.Default) where T : Slider => view.ProgressColor((Binding<Color>)color, state);
 
 		public static Color GetProgressColor(this Slider view, Color defaultColor = null, ControlState state = ControlState.Default)
 		{
@@ -155,6 +164,7 @@ namespace Comet
 			view.SetEnvironment(EnvironmentKeys.Slider.ThumbColor, color, cascades: false, state);
 			return view;
 		}
+		public static T ThumbColor<T>(this T view, Func<Color> color, ControlState state = ControlState.Default) where T : Slider => view.ThumbColor((Binding<Color>)color, state);
 
 		public static Color GetThumbColor(this Slider view, Color defaultColor = null, ControlState state = ControlState.Default)
 		{
@@ -178,6 +188,7 @@ namespace Comet
 			view.SetEnvironment(EnvironmentKeys.ProgressBar.TrackColor, color, cascades, state);
 			return view;
 		}
+		public static T TrackColor<T>(this T view, Func<Color> color, ControlState state = ControlState.Default, bool cascades = false) where T : ProgressBar => view.TrackColor((Binding<Color>)color, state, cascades);
 
 		public static Color GetTrackColor(this ProgressBar view, Color defaultColor = null, ControlState state = ControlState.Default)
 		{
@@ -192,6 +203,7 @@ namespace Comet
 			view.SetEnvironment(EnvironmentKeys.ProgressBar.ProgressColor, color, cascades);
 			return view;
 		}
+		public static T ProgressColor<T>(this T view, Func<Color> color, bool cascades = false) where T : ProgressBar => view.ProgressColor((Binding<Color>)color, cascades);
 
 		public static Color GetProgressColor(this ProgressBar view, Color defaultColor = null, ControlState state = ControlState.Default)
 		{
