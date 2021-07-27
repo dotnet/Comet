3b80063d34c782a661dd9c810ed5c04c0b0c32e7
James Clancey <james.clancey@gmail.com>
2021-07-26 16:42:15 -0800

    Added in ignore properties!

--- src/Comet/Controls/ControlsGenerator.cs
+++ src/Comet/Controls/ControlsGenerator.cs
@@ -6,5 +6,6 @@ using Comet;
 
 [assembly: CometGenerate(typeof(ILabel), "Text:Value", Namespace = "Comet", ClassName = "Text", DefaultValues = new[] { "MaxLines = 1" })]
 [assembly: CometGenerate(typeof(IButton), "Text", nameof(IButton.Clicked), Namespace = "Comet")]
+[assembly: CometGenerate(typeof(IEntry), "Text", "Placeholder", "Completed", ClassName = "SecureField", Namespace = "Comet", Skip = new[] { "IsPassword=true" })]
 
 
--- src/Comet/Controls/SecureField.cs
+++ src/Comet/Controls/SecureField.cs
@@ -1,97 +1,97 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.Maui;
-using Microsoft.Maui.Graphics;
+﻿//using System;
+//using System.Collections.Generic;
+//using Microsoft.Maui;
+//using Microsoft.Maui.Graphics;
 
-namespace Comet
-{
-	public class SecureField : View, IEntry
-	{
-		protected static Dictionary<string, string> TextHandlerPropertyMapper = new(HandlerPropertyMapper)
-		{
-			[nameof(Color)] = nameof(IText.TextColor),
-		};
-		public SecureField(
-			Binding<string> value = null,
-			Binding<string> placeholder = null,
-			Action<string> onCommit = null)
-		{
-			Text = value;
-			Placeholder = placeholder;
-			OnCommit = onCommit;
-		}
+//namespace Comet
+//{
+//	public class SecureField : View, IEntry
+//	{
+//		protected static Dictionary<string, string> TextHandlerPropertyMapper = new(HandlerPropertyMapper)
+//		{
+//			[nameof(Color)] = nameof(IText.TextColor),
+//		};
+//		public SecureField(
+//			Binding<string> value = null,
+//			Binding<string> placeholder = null,
+//			Action<string> onCommit = null)
+//		{
+//			Text = value;
+//			Placeholder = placeholder;
+//			OnCommit = onCommit;
+//		}
 
-		public SecureField(
-			Func<string> value,
-			Func<string> placeholder = null,
-			Action<string> onCommit = null) : this((Binding<string>)value, (Binding<string>)placeholder, onCommit)
-		{ }
+//		public SecureField(
+//			Func<string> value,
+//			Func<string> placeholder = null,
+//			Action<string> onCommit = null) : this((Binding<string>)value, (Binding<string>)placeholder, onCommit)
+//		{ }
 
-		Binding<string> _text;
-		public Binding<string> Text
-		{
-			get => _text;
-			private set => this.SetBindingValue(ref _text, value);
-		}
-
-
-		Binding<string> _placeholder;
-		public Binding<string> Placeholder
-		{
-			get => _placeholder;
-			private set => this.SetBindingValue(ref _placeholder, value);
-		}
-
-		public Action<string> OnCommit { get; set; }
+//		Binding<string> _text;
+//		public Binding<string> Text
+//		{
+//			get => _text;
+//			private set => this.SetBindingValue(ref _text, value);
+//		}
 
 
-		bool IEntry.IsPassword => true;
+//		Binding<string> _placeholder;
+//		public Binding<string> Placeholder
+//		{
+//			get => _placeholder;
+//			private set => this.SetBindingValue(ref _placeholder, value);
+//		}
 
-		//TODO: Expose these properties
-		bool ITextInput.IsTextPredictionEnabled => this.GetEnvironment<bool>(nameof(IEntry.IsTextPredictionEnabled));
+//		public Action<string> OnCommit { get; set; }
 
-		ReturnType IEntry.ReturnType => this.GetEnvironment<ReturnType>(nameof(IEntry.ReturnType));
 
-		ClearButtonVisibility IEntry.ClearButtonVisibility => this.GetEnvironment<ClearButtonVisibility>(nameof(IEntry.ClearButtonVisibility));
+//		bool IEntry.IsPassword => true;
 
-		string ITextInput.Text { get => Text; set => Text.Set(value); }
+//		//TODO: Expose these properties
+//		bool ITextInput.IsTextPredictionEnabled => this.GetEnvironment<bool>(nameof(IEntry.IsTextPredictionEnabled));
 
-		bool ITextInput.IsReadOnly => this.GetEnvironment<bool>(nameof(IEntry.IsReadOnly));
+//		ReturnType IEntry.ReturnType => this.GetEnvironment<ReturnType>(nameof(IEntry.ReturnType));
 
-		int ITextInput.MaxLength => this.GetEnvironment<int?>(nameof(IEntry.MaxLength)) ?? -1;
+//		ClearButtonVisibility IEntry.ClearButtonVisibility => this.GetEnvironment<ClearButtonVisibility>(nameof(IEntry.ClearButtonVisibility));
 
-		string IText.Text => Text;
+//		string ITextInput.Text { get => Text; set => Text.Set(value); }
 
-		Color ITextStyle.TextColor => this.GetColor();
+//		bool ITextInput.IsReadOnly => this.GetEnvironment<bool>(nameof(IEntry.IsReadOnly));
 
-		Font ITextStyle.Font => this.GetFont(null);
+//		int ITextInput.MaxLength => this.GetEnvironment<int?>(nameof(IEntry.MaxLength)) ?? -1;
 
-		double ITextStyle.CharacterSpacing => this.GetEnvironment<double>(nameof(IText.CharacterSpacing));
+//		string IText.Text => Text;
 
-		string IPlaceholder.Placeholder => this.Placeholder;
+//		Color ITextStyle.TextColor => this.GetColor();
 
-		TextAlignment ITextAlignment.HorizontalTextAlignment => this.GetTextAlignment() ?? TextAlignment.Start;
+//		Font ITextStyle.Font => this.GetFont(null);
 
-		Keyboard ITextInput.Keyboard => this.GetEnvironment<Keyboard>(nameof(ITextInput.Keyboard));
+//		double ITextStyle.CharacterSpacing => this.GetEnvironment<double>(nameof(IText.CharacterSpacing));
 
-		int IEntry.CursorPosition {
-			get => this.GetProperty<int>(cascades: false);
-			set => this.SetProperty(value, cascades: false);
-		}
-		int IEntry.SelectionLength
-		{
-			get => this.GetProperty<int>(cascades: false);
-			set => this.SetProperty(value, cascades: false);
-		}
+//		string IPlaceholder.Placeholder => this.Placeholder;
 
-		public void ValueChanged(string value)
-			=> OnCommit?.Invoke(value);
+//		TextAlignment ITextAlignment.HorizontalTextAlignment => this.GetTextAlignment() ?? TextAlignment.Start;
 
-		void IEntry.Completed() => OnCommit?.Invoke(Text);
+//		Keyboard ITextInput.Keyboard => this.GetEnvironment<Keyboard>(nameof(ITextInput.Keyboard));
 
-		protected override string GetHandlerPropertyName(string property)
-			=> TextHandlerPropertyMapper.TryGetValue(property, out var value) ? value : property;
+//		int IEntry.CursorPosition {
+//			get => this.GetProperty<int>(cascades: false);
+//			set => this.SetProperty(value, cascades: false);
+//		}
+//		int IEntry.SelectionLength
+//		{
+//			get => this.GetProperty<int>(cascades: false);
+//			set => this.SetProperty(value, cascades: false);
+//		}
 
-		TextAlignment ITextAlignment.VerticalTextAlignment => this.GetVerticalTextAlignment() ?? TextAlignment.Start;
-	}
-}
+//		public void ValueChanged(string value)
+//			=> OnCommit?.Invoke(value);
+
+//		void IEntry.Completed() => OnCommit?.Invoke(Text);
+
+//		protected override string GetHandlerPropertyName(string property)
+//			=> TextHandlerPropertyMapper.TryGetValue(property, out var value) ? value : property;
+
+//		TextAlignment ITextAlignment.VerticalTextAlignment => this.GetVerticalTextAlignment() ?? TextAlignment.Start;
+//	}
+//}
