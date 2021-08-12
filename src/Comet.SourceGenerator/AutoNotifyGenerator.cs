d00951b411ad44d5c163d21941ed8079e72801b0
James Clancey <james.clancey@gmail.com>
2021-08-11 23:27:03 -0800

    Updated the AutoNotifyGenerator to error when it can't generate! Added Test

--- src/Comet.SourceGenerator/AutoNotifyGenerator.cs
+++ src/Comet.SourceGenerator/AutoNotifyGenerator.cs
@@ -31,7 +31,7 @@ namespace Comet
 
 		public void Initialize(GeneratorInitializationContext context)
 		{
-			//Debugger.Launch();
+			context.RegisterForPostInitialization((i) => i.AddSource("AutoNotifyAttribute", attributeText));
 			// Register a syntax receiver that will be created for each generation pass
 			context.RegisterForSyntaxNotifications(() => new SyntaxReceiver());
 		}
@@ -39,50 +39,37 @@ namespace Comet
 		public void Execute(GeneratorExecutionContext context)
 		{
 			// add the attribute text
-			context.AddSource("AutoNotifyAttribute", SourceText.From(attributeText, Encoding.UTF8));
 
 			// retreive the populated receiver 
-			if (!(context.SyntaxReceiver is SyntaxReceiver receiver))
+			if (!(context.SyntaxContextReceiver is SyntaxReceiver receiver) || !receiver.Fields.Any())
 				return;
-
 			// we're going to create a new compilation that contains the attribute.
 			// TODO: we should allow source generators to provide source during initialize, so that this step isn't required.
 			CSharpParseOptions options = (context.Compilation as CSharpCompilation).SyntaxTrees[0].Options as CSharpParseOptions;
 			Compilation compilation = context.Compilation.AddSyntaxTrees(CSharpSyntaxTree.ParseText(SourceText.From(attributeText, Encoding.UTF8), options));
 
-			// get the newly bound attribute, and INotifyPropertyChanged
-			INamedTypeSymbol attributeSymbol = compilation.GetTypeByMetadataName("Comet.AutoNotifyAttribute");
-			INamedTypeSymbol notifySymbol = compilation.GetTypeByMetadataName("System.ComponentModel.INotifyPropertyChanged");
+
+			var attributeSymbol = context.Compilation.GetTypeByMetadataName("Comet.AutoNotifyAttribute");
+			var notifySymbol = context.Compilation.GetTypeByMetadataName("System.ComponentModel.INotifyPropertyChanged");
 			var notifyReadSymbol = compilation.GetTypeByMetadataName("Comet.INotifyPropertyRead");
 			var autoImplementedSymbol = compilation.GetTypeByMetadataName("Comet.IAutoImplemented");
-			// loop over the candidate fields, and keep the ones that are actually annotated
-			List<IFieldSymbol> fieldSymbols = new List<IFieldSymbol>();
-			foreach (FieldDeclarationSyntax field in receiver.CandidateFields)
-			{
-				SemanticModel model = compilation.GetSemanticModel(field.SyntaxTree);
-				foreach (VariableDeclaratorSyntax variable in field.Declaration.Variables)
-				{
-					// Get the symbol being decleared by the field, and keep it if its annotated
-					IFieldSymbol fieldSymbol = model.GetDeclaredSymbol(variable) as IFieldSymbol;
-					if (fieldSymbol.GetAttributes().Any(ad => ad.AttributeClass.Equals(attributeSymbol, SymbolEqualityComparer.Default)))
-					{
-						fieldSymbols.Add(fieldSymbol);
-					}
-				}
-			}
 
 			// group the fields by class, and generate the source
-			foreach (IGrouping<INamedTypeSymbol, IFieldSymbol> group in fieldSymbols.GroupBy(f => f.ContainingType))
+			foreach (IGrouping<INamedTypeSymbol, IFieldSymbol> group in receiver.Fields.GroupBy(f => f.ContainingType))
 			{
 				string classSource = ProcessClass(group.Key, group.ToList(), attributeSymbol, notifySymbol, notifyReadSymbol, autoImplementedSymbol, context);
-				context.AddSource($"{group.Key.Name}_autoNotify.cs", SourceText.From(classSource, Encoding.UTF8));
+				if(!string.IsNullOrWhiteSpace(classSource))
+					context.AddSource($"{group.Key.Name}_autoNotify.cs", SourceText.From(classSource, Encoding.UTF8));
 			}
-		}
 
+		
+		
+		}
 		private string ProcessClass(INamedTypeSymbol classSymbol, List<IFieldSymbol> fields, ISymbol attributeSymbol, ISymbol notifySymbol, ISymbol notifyReadSymbol, ISymbol autoImplementedSymbol, GeneratorExecutionContext context)
 		{
 			if (!classSymbol.ContainingSymbol.Equals(classSymbol.ContainingNamespace, SymbolEqualityComparer.Default))
 			{
+				context.ReportDiagnostic(Diagnostic.Create("AutoGen101", "Compiler", message: $"{classSymbol.ToDisplayString()} cannot be a nested class in order to use the [AutoNotify] attribute.", DiagnosticSeverity.Error, defaultSeverity: DiagnosticSeverity.Error,true,0)) ;
 				return null; //TODO: issue a diagnostic that it must be top level
 			}
 
@@ -92,7 +79,7 @@ namespace Comet
 			StringBuilder source = new StringBuilder($@"
 using Comet;
 namespace {namespaceName}
-{{ 
+{{
     public partial class {classSymbol.Name} : {notifyReadSymbol.ToDisplayString()} , {autoImplementedSymbol.ToDisplayString()}
     {{
 ");
@@ -102,6 +89,7 @@ namespace {namespaceName}
 			{
 				source.Append("public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;");
 			}
+			// if the class doesn't implement INotifyPropertyChanged already, add it
 			if (!classSymbol.Interfaces.Contains(notifyReadSymbol))
 			{
 				source.Append("public event System.ComponentModel.PropertyChangedEventHandler PropertyRead;");
@@ -143,7 +131,6 @@ public {fieldType} {propertyName}
 		this.PropertyRead?.Invoke(this, new System.ComponentModel.PropertyChangedEventArgs(nameof({propertyName})));
         return this.{fieldName};
     }}
-
     set
     {{
         this.{fieldName} = value;
@@ -151,7 +138,6 @@ public {fieldType} {propertyName}
         this.PropertyChanged?.Invoke(this, new System.ComponentModel.PropertyChangedEventArgs(nameof({propertyName})));
     }}
 }}
-
 ");
 
 			string chooseName(string fieldName, TypedConstant overridenNameOpt)
@@ -176,20 +162,28 @@ public {fieldType} {propertyName}
 		/// <summary>
 		/// Created on demand before each generation pass
 		/// </summary>
-		class SyntaxReceiver : ISyntaxReceiver
+		class SyntaxReceiver : ISyntaxContextReceiver
 		{
-			public List<FieldDeclarationSyntax> CandidateFields { get; } = new List<FieldDeclarationSyntax>();
+			public List<IFieldSymbol> Fields { get; } = new List<IFieldSymbol>();
 
 			/// <summary>
 			/// Called for every syntax node in the compilation, we can inspect the nodes and save any information useful for generation
 			/// </summary>
-			public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+			public void OnVisitSyntaxNode(GeneratorSyntaxContext context)
 			{
 				// any field with at least one attribute is a candidate for property generation
-				if (syntaxNode is FieldDeclarationSyntax fieldDeclarationSyntax
+				if (context.Node is FieldDeclarationSyntax fieldDeclarationSyntax
 					&& fieldDeclarationSyntax.AttributeLists.Count > 0)
 				{
-					CandidateFields.Add(fieldDeclarationSyntax);
+					foreach (VariableDeclaratorSyntax variable in fieldDeclarationSyntax.Declaration.Variables)
+					{
+						// Get the symbol being declared by the field, and keep it if its annotated
+						IFieldSymbol fieldSymbol = context.SemanticModel.GetDeclaredSymbol(variable) as IFieldSymbol;
+						if (fieldSymbol.GetAttributes().Any(ad => ad.AttributeClass.ToDisplayString() == "Comet.AutoNotifyAttribute"))
+						{
+							Fields.Add(fieldSymbol);
+						}
+					}
 				}
 			}
 		}
---
+++ tests/Comet.Tests/AutoNotifyTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Comet;
+using Xunit;
+
+namespace Comet.Tests
+{
+	public partial class StringConcatanator
+	{
+		[AutoNotify]
+		string value1;
+
+		[AutoNotify]
+		string value2;
+
+		public string Total => $"{Value1}{Value2}";
+	}
+
+	public class AutoNotifyTest : TestBase
+	{
+
+		public class MainPage : View
+		{
+
+			[State]
+			public readonly StringConcatanator model = new();
+			public Text TotalText { get; set; }
+			[Body]
+			View body()
+				=> new VStack
+				{
+					(TotalText = new Text(model.Total))
+				};
+
+		
+		}
+
+
+		[Fact]
+		public void VerifyAutoGeneratesAndUpdates()
+		{
+			var view = new MainPage();		
+			view.SetViewHandlerToGeneric();
+			view.model.Value1 = "Foo";
+			view.model.Value2 = "Bar";
+
+			Assert.Equal("FooBar", view.TotalText.Value);
+
+		}
+
+	}
+}
