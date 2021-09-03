674b7bed5e107171605c98d9c6a12c9eec6eac07
James Clancey <james.clancey@gmail.com>
2021-09-02 19:45:40 -0800

    Started fixing label fonts

--- src/Comet/Controls/ControlsGenerator.cs
+++ src/Comet/Controls/ControlsGenerator.cs
@@ -4,7 +4,7 @@ using System.Text;
 using Microsoft.Maui;
 using Comet;
 //Property:NewPropertyName=DefaultValue
-[assembly: CometGenerate(typeof(ILabel), $"{nameof(ILabel.Text)}:Value", Namespace = "Comet", ClassName = "Text", DefaultValues = new[] { "MaxLines = 1" })]
+[assembly: CometGenerate(typeof(ILabel), $"{nameof(ILabel.Text)}:Value", Namespace = "Comet", ClassName = "Text", Skip = new[] { $"{nameof(ITextStyle.Font)}=this.GetFont(null)" }, DefaultValues = new[] { "MaxLines = 1" })]
 [assembly: CometGenerate(typeof(IButton), nameof(IButton.Text), nameof(IButton.Clicked), Namespace = "Comet")]
 [assembly: CometGenerate(typeof(IEntry), nameof(IEntry.Text), nameof(IEntry.Placeholder), nameof(IEntry.Completed), ClassName = "SecureField", Namespace = "Comet", Skip = new[] { $"{nameof(IEntry.IsPassword)}= true" })]
 [assembly: CometGenerate(typeof(IActivityIndicator), Namespace ="Comet", Skip = new[] { $"{nameof(IActivityIndicator.IsRunning)}=true" })]
