d8a870faeb9e5cfc9488427040b22d0344c00af1
James Clancey <james.clancey@gmail.com>
2021-09-07 15:21:27 -0800

    Fixed text alignment

--- src/Comet/Controls/ControlsGenerator.cs
+++ src/Comet/Controls/ControlsGenerator.cs
@@ -4,7 +4,7 @@ using System.Text;
 using Microsoft.Maui;
 using Comet;
 //Property:NewPropertyName=DefaultValue
-[assembly: CometGenerate(typeof(ILabel), $"{nameof(ILabel.Text)}:Value", Namespace = "Comet", ClassName = "Text", DefaultValues = new[] { "MaxLines = 1" })]
+[assembly: CometGenerate(typeof(ILabel), $"{nameof(ILabel.Text)}:Value", Namespace = "Comet", ClassName = "Text", DefaultValues = new[] { "MaxLines = 1" }, Skip = new[] { $"{nameof(ITextAlignment.HorizontalTextAlignment)}", $"{nameof(ITextAlignment.VerticalTextAlignment)}" })]
 [assembly: CometGenerate(typeof(IButton), nameof(IButton.Text), nameof(IButton.Clicked), Namespace = "Comet")]
 [assembly: CometGenerate(typeof(IEntry), nameof(IEntry.Text), nameof(IEntry.Placeholder), nameof(IEntry.Completed), ClassName = "SecureField", Namespace = "Comet", Skip = new[] { $"{nameof(IEntry.IsPassword)}= true" })]
 [assembly: CometGenerate(typeof(IActivityIndicator), Namespace ="Comet", Skip = new[] { $"{nameof(IActivityIndicator.IsRunning)}=true" })]
