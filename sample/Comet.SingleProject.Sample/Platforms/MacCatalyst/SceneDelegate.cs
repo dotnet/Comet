a8a279435b734bd5bcbea7d3f98d5492cc1153ab
James Clancey <james.clancey@gmail.com>
2021-11-09 12:16:23 -0900

    Added scenedelegates to iOS

---
+++ sample/Comet.SingleProject.Sample/Platforms/MacCatalyst/SceneDelegate.cs
@@ -0,0 +1,15 @@
+﻿using Foundation;
+using Microsoft.Maui;
+using ObjCRuntime;
+using UIKit;
+
+namespace CometSample
+{
+	[Register("SceneDelegate")]
+	public class SceneDelegate : MauiUISceneDelegate
+	{
+
+
+	}
+}
+
---
+++ sample/Comet.iOS.Sample/SceneDelegate.cs
@@ -0,0 +1,15 @@
+﻿using Foundation;
+using Microsoft.Maui;
+using ObjCRuntime;
+using UIKit;
+
+namespace Comet.iOS.Sample
+{
+	[Register("SceneDelegate")]
+	public class SceneDelegate : MauiUISceneDelegate
+	{
+
+
+	}
+}
+
