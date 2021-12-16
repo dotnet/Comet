85f3a3355d646d2669bbf7bf4cd24d7b5eeed183
James Clancey <james.clancey@gmail.com>
2021-12-16 14:29:41 -0900

    Final fix to android template

--- templates/single-project/CometApp1/Platforms/Android/MainApplication.cs
+++ templates/single-project/CometApp1/Platforms/Android/MainApplication.cs
@@ -1,14 +1,14 @@
 ﻿using Android.App;
 using Android.Runtime;
+using Microsoft.Maui.Hosting;
 
 namespace CometApp1;
 
 [Application]
 public class MainApplication : MauiApplication
 {
-	public MainApplication(IntPtr handle, JniHandleOwnership ownership)
-		: base(handle, ownership)
+	public MainApplication(IntPtr handle, JniHandleOwnership ownership)	: base(handle, ownership)
 	{
-		protected override MauiApp CreateMauiApp() => App.CreateMauiApp();
 	}
+	protected override MauiApp CreateMauiApp() => App.CreateMauiApp();
 }
