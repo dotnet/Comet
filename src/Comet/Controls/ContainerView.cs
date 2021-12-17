4b1adc62296b770f656cc4b6401850ee2cf2a349
James Clancey <james.clancey@gmail.com>
2021-12-16 19:50:04 -0900

    Added working api for IEnumerables

--- src/Comet/Controls/ContainerView.cs
+++ src/Comet/Controls/ContainerView.cs
@@ -9,16 +9,30 @@ namespace Comet
 	public class ContainerView : View, IList<View>, IContainerView, IContainer
 	{
 		readonly protected List<View> Views = new List<View>();
-
-		public void Add(IEnumerable<View> views)
+		public void Add(object obj)
 		{
-			foreach (var v in views)
-				Add(v);
-		}
-		public void Add(IEnumerable<IView> views)
-		{
-			foreach (var v in views)
-				Add(v);
+			if (obj is View vv)
+			{
+				Add(vv);
+			}
+			else if(obj is IView iv)
+			{
+				Add(iv);
+			}
+			else if(obj is IEnumerable<View> iev)
+			{
+				foreach (var v in iev)
+					Add(v);
+			}
+			else if(obj is IEnumerable<IView> ieiv)
+			{
+				foreach (var v in ieiv)
+					Add(v);
+			}
+			else
+			{
+				throw new NotSupportedException("The object either needs to be a View, IView, or IEnumerable<IView>/IEnumerable<View>");
+			}
 		}
 		public void Add(IView iView)
 		{
