cf10b826d7bac32f9c6114e8b5bfd4875b752344
James Clancey <james.clancey@gmail.com>
2021-12-16 21:05:35 -0900

    Fixed Button Text Color

--- src/Comet/Controls/ControlsGenerator.cs
+++ src/Comet/Controls/ControlsGenerator.cs
@@ -4,7 +4,7 @@ using System.Text;
 using Microsoft.Maui;
 using Comet;
 //Property:NewPropertyName=DefaultValue
-[assembly: CometGenerate(typeof(ITextButton), nameof(ITextButton.Text), nameof(IButton.Clicked), ClassName = "Button", Namespace = "Comet")]
+[assembly: CometGenerate(typeof(ITextButton), nameof(ITextButton.Text), nameof(IButton.Clicked), ClassName = "Button", Skip = new[] { $"{nameof(ITextButton.TextColor}:{EnvironmentKeys.Colors.Color}" }, Namespace = "Comet")]
 //[assembly: CometGenerate(typeof(IImageButton), nameof(IImageButton.Source), nameof(IButton.Clicked), ClassName = "ImageButton", Namespace = "Comet")]
 //[assembly: CometGenerate(typeof(IBorder), BaseClass = "ContentView", Namespace = "Comet")]
 [assembly: CometGenerate(typeof(IIndicatorView),nameof(IIndicatorView.Count), ClassName = "IndicatorView", Namespace = "Comet")]
