cc81557113ebf28166d894710a7433e6d47bd53d
James Clancey <james.clancey@gmail.com>
2022-01-11 14:52:37 -0900

    Android scrollview now lays out

--- src/Comet/Handlers/ScrollView/ScrollViewHandler.Android.cs
+++ src/Comet/Handlers/ScrollView/ScrollViewHandler.Android.cs
@@ -1,6 +1,6 @@
 ﻿using Android.Content;
 using AView = Android.Views.View;
-using AScrollView = Android.Widget.ScrollView;
+using AScrollView = Comet.Android.Controls.CometScrollView;
 using Microsoft.Maui.Handlers;
 using Microsoft.Maui;
 using static Android.Views.ViewGroup;
@@ -12,7 +12,10 @@ namespace Comet.Handlers
 
 		private AView _content;
 
-		protected override AScrollView CreateNativeView() => new AScrollView(Context);
+		protected override AScrollView CreateNativeView() => new AScrollView(Context)
+		{
+			CrossPlatformArrange = Arange,
+		};
 		
 		protected override void DisconnectHandler(AScrollView view)
 		{
@@ -23,6 +26,23 @@ namespace Comet.Handlers
 			}
 
 			base.DisconnectHandler(view);
+		}
+
+		void Arange(Rectangle rect)
+		{
+			var sizeAllowed = this.VirtualView.Orientation == Orientation.Vertical ? new Size(rect.Width, double.MaxValue) : new Size(double.MaxValue, rect.Height);
+			var measuredSize = VirtualView?.Content?.Measure(sizeAllowed.Width, sizeAllowed.Height) ?? Size.Zero;
+			//Make sure we at least fit the scroll view
+			if (double.IsInfinity(measuredSize.Width))
+				measuredSize.Width = rect.Width;
+			if (double.IsInfinity(measuredSize.Height))
+				measuredSize.Height = rect.Height;
+			measuredSize.Width = Math.Max(measuredSize.Width, rect.Width);
+			measuredSize.Height = Math.Max(measuredSize.Height, rect.Height);
+			if (VirtualView?.Content != null)
+				VirtualView.Content.Frame = new Rectangle(Point.Zero, measuredSize);
+			//NativeView.v = measuredSize.ToCGSize();
+			//_content.Frame = new CGRect(CGPoint.Empty, measuredSize);
 		}
 
 		public override void SetVirtualView(IView view)
---
+++ src/Comet/Platform/Android/CometScrollView.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.Content;
+using AScrollView = Android.Widget.ScrollView;
+
+namespace Comet.Android.Controls
+{
+	public class CometScrollView : AScrollView
+	{
+		public CometScrollView(Context context) : base(context)
+		{
+		}
+
+		internal Action<Rectangle> CrossPlatformArrange { get; set; }
+
+		protected override void OnLayout(bool changed, int left, int top, int right, int bottom)
+		{
+			if (CrossPlatformArrange == null || Context == null)
+			{
+				return;
+			}
+
+			var deviceIndependentLeft = Context.FromPixels(left);
+			var deviceIndependentTop = Context.FromPixels(top);
+			var deviceIndependentRight = Context.FromPixels(right);
+			var deviceIndependentBottom = Context.FromPixels(bottom);
+
+			var destination = Rectangle.FromLTRB(0, 0,
+				deviceIndependentRight - deviceIndependentLeft, deviceIndependentBottom - deviceIndependentTop);
+
+			CrossPlatformArrange(destination);
+		}
+	}
+}
