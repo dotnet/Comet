af3f2405c01788cf4ea52caf7ba0ea5f96d703fe
James Clancey <james.clancey@gmail.com>
2022-01-10 15:04:02 -0900

    Trying to make Comet-net5.debug reference Maui source

--- sample/Comet.SingleProject.Sample/Platforms/iOS/AppDelegate.cs
+++ sample/Comet.SingleProject.Sample/Platforms/iOS/AppDelegate.cs
@@ -8,6 +8,8 @@ namespace CometSample
 	[Register("AppDelegate")]
 	public class AppDelegate : MauiUIApplicationDelegate
 	{
+#pragma warning disable CA1416 // Validate platform compatibility
 		protected override MauiApp CreateMauiApp() => MyApp.CreateMauiApp();
+#pragma warning restore CA1416 // Validate platform compatibility
 	}
 }
