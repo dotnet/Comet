41fa66559b1c7f7c8cc2c162f102765b6981a9c5
James Clancey <james.clancey@gmail.com>
2022-03-04 16:40:46 -0900

    Automatically update the Maui Version based on what was used to build.

---
+++ build/MauiVersionUpdater/Program.cs
@@ -0,0 +1,55 @@
+﻿
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+var url = "https://aka.ms/dotnet/maui/main.json";
+var path = "Directory.Build.props";
+if (args.Length > 0)
+{
+	url = args[0];
+}
+
+Console.WriteLine($"Parsing url: {url}");
+var json = await new HttpClient().GetStringAsync(url);
+
+var versionInfo = JsonSerializer.Deserialize<RollBackModel>(json);
+
+Console.WriteLine($"MauiVersion {versionInfo.MicrosoftNetSdkMaui}");
+Console.WriteLine($"Current Directory {Directory.GetCurrentDirectory()}");
+
+var buildProps = File.ReadAllText(path);
+var openString = "<MauiVersion>";
+var start = buildProps.IndexOf(openString);
+start += openString.Length;
+var end = buildProps.IndexOf("</MauiVersion>");
+var oldValue = buildProps.Substring(start, end - start);
+Console.WriteLine($"Old Value: {oldValue}");
+var newProps = buildProps.Replace(oldValue, versionInfo.MicrosoftNetSdkMaui);
+File.WriteAllText(path, newProps);
+
+public class RollBackModel
+{
+	[JsonPropertyName("microsoft.net.sdk.android")]
+	public string MicrosoftNetSdkAndroid { get; set; }
+
+	[JsonPropertyName("microsoft.net.sdk.ios")]
+	public string MicrosoftNetSdkIos { get; set; }
+
+	[JsonPropertyName("microsoft.net.sdk.maccatalyst")]
+	public string MicrosoftNetSdkMaccatalyst { get; set; }
+
+	[JsonPropertyName("microsoft.net.sdk.macos")]
+	public string MicrosoftNetSdkMacos { get; set; }
+
+	[JsonPropertyName("microsoft.net.sdk.maui")]
+	public string MicrosoftNetSdkMaui { get; set; }
+
+	[JsonPropertyName("microsoft.net.sdk.tvos")]
+	public string MicrosoftNetSdkTvos { get; set; }
+
+	[JsonPropertyName("microsoft.net.workload.mono.toolchain")]
+	public string MicrosoftNetWorkloadMonoToolchain { get; set; }
+
+	[JsonPropertyName("microsoft.net.workload.emscripten")]
+	public string MicrosoftNetWorkloadEmscripten { get; set; }
+}
