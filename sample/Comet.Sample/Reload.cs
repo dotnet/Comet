39e187a9294a7a0ad55012d5699c3398b17cbc57
Jesse <jessejiang0214@outlook.com>
2022-12-01 15:12:42 +1100

    fix hot reload server did launch when debug start

--- sample/Comet.Sample/Reload.cs
+++ sample/Comet.Sample/Reload.cs
@@ -40,6 +40,8 @@ namespace Comet
 
 			public int IdePort { get; set; } = 9988;
 
+			int _attempts = 3;
+
 			public async void Initialize(IServiceProvider services)
 			{
 				var handlers = services.GetRequiredService<IMauiHandlersFactory>();
@@ -59,6 +61,14 @@ namespace Comet
 					try
 					{
 						var success = await Reloadify.Reload.Init(IdeIp, IdePort);
+
+						while(!success && _attempts > 0)
+						{
+							await Task.Delay(TimeSpan.FromSeconds(5));
+							Console.WriteLine("Trying to connect HotReload server....");
+							success = await Reloadify.Reload.Init(IdeIp, IdePort);
+							_attempts--;
+						}
 
 						Console.WriteLine($"HotReload Initialize: {success}");
 					}
